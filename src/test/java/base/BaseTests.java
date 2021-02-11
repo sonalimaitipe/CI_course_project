5c8ec5e0a1f58955e48e8bb4e46765a42e00ea3a
sonalimaitipe <sonalimaitipe97@gmail.com>
2021-02-12 00:40:52 +0530

    test class created for login

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,15 +2,18 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://testautomationu.applitools.com/");
+        homePage = new HomePage(driver);
         driver.manage().window().maximize();
     }
 }
---
+++ src/test/java/login/LoginTest.java
@@ -0,0 +1,14 @@
+package login;
+
+import base.BaseTests;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTests {
+
+    public void testSuccessFullLogin() {
+        LoginPage loginPage = homePage.clickEnrollButton();
+        
+
+    }
+
+}
